63b559a9ba7a6a4f35e4566770a12273d4ffb81f
Wayne Chen <mili20507keku@gmail.com>
2020-10-05 13:08:53 +0200

    1

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
